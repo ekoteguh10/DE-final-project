e871a89ccdb731a6d18d98fc962c906688282990
Eko Teguh Widodo <id.ekoteguh@gmail.com>
2022-03-08 00:02:07 +0700

    update spark file: update README

--- airflow-docker/spark_files/extract_to_parquet.py
+++ airflow-docker/spark_files/extract_to_parquet.py
@@ -9,27 +9,27 @@ spark = SparkSession \
 
 # Before transforming, we need to read datasets
 # After then, transform into PARQUET format files
-df_country = spark.read.options(header="true", inferSchema="true").csv("gs://final_project_ekoteguh/INPUT/USCOUNTRY.csv")
-df_country.write.mode("Overwrite").parquet("gs://final_project_ekoteguh/RAW/uscountry.parquet")
+df_country = spark.read.options(header="true", inferSchema="true").csv("gs://final-project-ekoteguh/INPUT/USCOUNTRY.csv")
+df_country.write.mode("Overwrite").parquet("gs://final-project-ekoteguh/RAW/uscountry.parquet")
 
-df_port = spark.read.options(header="true", inferSchema="true").csv("gs://final_project_ekoteguh/INPUT/USPORT.csv")
-df_port.write.mode("Overwrite").parquet("gs://final_project_ekoteguh/RAW/usport.parquet")
+df_port = spark.read.options(header="true", inferSchema="true").csv("gs://final-project-ekoteguh/INPUT/USPORT.csv")
+df_port.write.mode("Overwrite").parquet("gs://final-project-ekoteguh/RAW/usport.parquet")
 
-df_state = spark.read.options(header="true", inferSchema="true").csv("gs://final_project_ekoteguh/INPUT/USSTATE.csv")
-df_state.write.mode("Overwrite").parquet("gs://final_project_ekoteguh/RAW/usstate.parquet")
+df_state = spark.read.options(header="true", inferSchema="true").csv("gs://final-project-ekoteguh/INPUT/USSTATE.csv")
+df_state.write.mode("Overwrite").parquet("gs://final-project-ekoteguh/RAW/usstate.parquet")
 
-df_airport = spark.read.options(header="true", inferSchema="true").csv("gs://final_project_ekoteguh/INPUT/airport-codes_csv.csv")
-df_airport.write.mode("Overwrite").parquet("gs://final_project_ekoteguh/RAW/airportcodes.parquet")
+df_airport = spark.read.options(header="true", inferSchema="true").csv("gs://final-project-ekoteguh/INPUT/airport-codes_csv.csv")
+df_airport.write.mode("Overwrite").parquet("gs://final-project-ekoteguh/RAW/airportcodes.parquet")
 
-df_immigration = spark.read.options(header="true", inferSchema="true").csv("gs://final_project_ekoteguh/INPUT/immigration_data_sample.csv")
-df_immigration.write.mode("Overwrite").parquet("gs://final_project_ekoteguh/RAW/immigration.parquet")
+df_immigration = spark.read.options(header="true", inferSchema="true").csv("gs://final-project-ekoteguh/INPUT/immigration_data_sample.csv")
+df_immigration.write.mode("Overwrite").parquet("gs://final-project-ekoteguh/RAW/immigration.parquet")
 
-df_temp = spark.read.options(header="true", inferSchema="true").csv("gs://final_project_ekoteguh/INPUT/GlobalLandTemperaturesByCity.csv")
-df_temp.write.mode("Overwrite").parquet("gs://final_project_ekoteguh/RAW/globaltempbycity.parquet")
+df_temp = spark.read.options(header="true", inferSchema="true").csv("gs://final-project-ekoteguh/INPUT/GlobalLandTemperaturesByCity.csv")
+df_temp.write.mode("Overwrite").parquet("gs://final-project-ekoteguh/RAW/globaltempbycity.parquet")
 
 # The delimiter for this file (us-cities-demographics.csv) is semicolon (;)
 # We have to change the default delimiter
-df_demographic = spark.read.options(header="true", inferSchema="true", delimiter=";").csv("gs://final_project_ekoteguh/INPUT/us-cities-demographics.csv")
+df_demographic = spark.read.options(header="true", inferSchema="true", delimiter=";").csv("gs://final-project-ekoteguh/INPUT/us-cities-demographics.csv")
 df_demographic.printSchema()
 # There is a problem in Header of this df, 
 # Therefore, we need to rename the header before write it into parquet file
@@ -40,4 +40,4 @@ demographic_new_col_names = ["City", "State", "MedianAge", "MalePopulation", "Fe
 df_demographic = df_demographic.toDF(*demographic_new_col_names)
 
 # Then, we convert it into parquet
-df_demographic.write.mode("Overwrite").parquet("gs://final_project_ekoteguh/RAW/demographic.parquet")
+df_demographic.write.mode("Overwrite").parquet("gs://final-project-ekoteguh/RAW/demographic.parquet")
